95d6b2b121c5cd4d18f636c3fee8acd4098328a2
sfkarbo <sfkarbo@3a0173bb-499f-41c5-b03c-56d250de409b>
2009-01-20 21:11:41 +0000

    Initial import.
    git-svn-id: https://ita.svn.sourceforge.net/svnroot/ita/itunesagent/trunk/itunesagent@15 3a0173bb-499f-41c5-b03c-56d250de409b

---
+++ CryptoUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Jaranweb.iTunesAgent
+{
+    /// <summary>
+    /// Contains utiliy methods supporting the System.Security.Cryptography namespace.
+    /// </summary>
+    public class CryptoUtils
+    {
+        /// <summary>
+        /// Compute the MD5 hash and return it in hex format.
+        /// </summary>
+        /// <param name="original">The original string to compute the MD5 hash for.</param>
+        /// <returns>MD5 hex for the original string.</returns>
+        public static string Md5Hex(string original)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] hash = md5.ComputeHash(Encoding.Default.GetBytes(original));
+            StringBuilder sb = new StringBuilder(32);
+            foreach (byte b in hash)
+            {
+                sb.Append(b.ToString("x2").ToUpper());
+            }
+            return sb.ToString().ToLower();
+        }
+    }
+}
