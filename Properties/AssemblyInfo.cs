a5055735cfbc6425548daec3773532b215d884e5
sfkarbo <sfkarbo@3a0173bb-499f-41c5-b03c-56d250de409b>
2010-01-04 20:56:28 +0000

    Build number increment.
    git-svn-id: https://ita.svn.sourceforge.net/svnroot/ita/itunesagent/trunk/itunesagent@144 3a0173bb-499f-41c5-b03c-56d250de409b

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.2.113")]
+[assembly: AssemblyVersion("1.3.2.114")]
 [assembly: AssemblyFileVersion("1.3.2")]
