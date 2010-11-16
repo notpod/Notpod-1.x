c6a8e16d84a9499f9106b8f0c12012d69476666f
sfkarbo <sfkarbo@3a0173bb-499f-41c5-b03c-56d250de409b>
2010-11-16 12:08:25 +0000

    Updated version information.
    git-svn-id: https://ita.svn.sourceforge.net/svnroot/ita/itunesagent/trunk/itunesagent@215 3a0173bb-499f-41c5-b03c-56d250de409b

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.4.168")]
+[assembly: AssemblyVersion("1.3.4")]
 [assembly: AssemblyFileVersion("1.3.4")]
