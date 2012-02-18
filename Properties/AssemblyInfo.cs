806c9a1f8c0e9da7f152cd059005dd1d130aa634
Jaran Nilsen <jaran.nilsen@gmail.com>
2012-02-18 15:14:11 +0100

    set description

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,12 +1,14 @@
+
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Notpod")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("BETA")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jaran Nilsen")]
 [assembly: AssemblyProduct("Notpod")]
@@ -29,5 +31,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.1")]
-[assembly: AssemblyFileVersion("1.4.1")]
+[assembly: AssemblyVersion("1.5")]
+[assembly: AssemblyFileVersion("1.5")]
+[assembly: AssemblyInformationalVersion("BETA")]
