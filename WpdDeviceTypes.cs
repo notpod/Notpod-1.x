fc415556df8fdec1b5a55fa286b0de15ff1af673
Jaran Nilsen <jaran.nilsen@gmail.com>
2013-01-05 16:00:12 +0100

    added constants for different device types.

---
+++ WpdDeviceTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Notpod
+{
+    public sealed class WpdDeviceTypes
+    {
+        public static readonly int WPD_DEVICE_TYPE_GENERIC = 0;
+        public static readonly int WPD_DEVICE_TYPE_CAMERA = 1;
+        public static readonly int WPD_DEVICE_TYPE_MEDIA_PLAYER = 2;
+        public static readonly int WPD_DEVICE_TYPE_PHONE = 3;
+    }
+}
